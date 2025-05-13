c6b8f93d9637ca7744adc9fd002e4193cfbedc67
Amadou M <amadoumaiga7002@gmail.com>
2025-05-13 13:09:48 +0000

    test de defferent port

---
+++ server/config/setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': '5432',
+    }
+}
+
